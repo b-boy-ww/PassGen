59a026debb8e93b6c774437681f37c8d67368116
William M-w <williammw.wmw@gmail.com>
2018-04-13 23:10:24 +0100

    Added semi-Functional manual password/applet input.

--- PassGen/PassGen/Form1.Designer.cs
+++ PassGen/PassGen/Form1.Designer.cs
@@ -30,14 +30,23 @@
             this.label2 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkbox = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.AcctName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AccntPass = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // NameBox
             // 
-            this.NameBox.Location = new System.Drawing.Point(218, 66);
+            this.NameBox.Location = new System.Drawing.Point(243, 66);
             this.NameBox.Name = "NameBox";
             this.NameBox.Size = new System.Drawing.Size(513, 20);
             this.NameBox.TabIndex = 0;
@@ -45,7 +54,7 @@
             // PWbox
             // 
             this.PWbox.Enabled = false;
-            this.PWbox.Location = new System.Drawing.Point(218, 107);
+            this.PWbox.Location = new System.Drawing.Point(243, 107);
             this.PWbox.Name = "PWbox";
             this.PWbox.Size = new System.Drawing.Size(513, 20);
             this.PWbox.TabIndex = 0;
@@ -54,7 +63,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(81, 111);
+            this.label1.Location = new System.Drawing.Point(116, 111);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(96, 13);
             this.label1.TabIndex = 2;
@@ -63,7 +72,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(90, 70);
+            this.label2.Location = new System.Drawing.Point(125, 70);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 13);
             this.label2.TabIndex = 2;
@@ -90,15 +99,6 @@
             this.button1.Text = "Generate Password";
             this.button1.UseVisualStyleBackColor = false;
             // 
-            // listView1
-            // 
-            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.listView1.Location = new System.Drawing.Point(30, 245);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(758, 164);
-            this.listView1.TabIndex = 6;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(316, 415);
@@ -117,6 +117,78 @@
             this.button3.TabIndex = 4;
             this.button3.Text = "Save Login Details";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Location = new System.Drawing.Point(13, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(93, 79);
+            this.panel1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Preferences";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(14, 18);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(68, 17);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "Numbers";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(3, 48);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(91, 17);
+            this.checkBox3.TabIndex = 0;
+            this.checkBox3.Text = "Special Chars";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.checkbox,
+            this.AcctName,
+            this.AccntPass});
+            this.dataGridView1.Location = new System.Drawing.Point(17, 269);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(774, 140);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // checkbox
+            // 
+            this.checkbox.HeaderText = "";
+            this.checkbox.Name = "checkbox";
+            this.checkbox.Width = 25;
+            // 
+            // AcctName
+            // 
+            this.AcctName.HeaderText = "Account Name";
+            this.AcctName.Name = "AcctName";
+            this.AcctName.ReadOnly = true;
+            // 
+            // AccntPass
+            // 
+            this.AccntPass.HeaderText = "Account Password";
+            this.AccntPass.Name = "AccntPass";
+            this.AccntPass.ReadOnly = true;
             // 
             // Form1
             // 
@@ -124,8 +196,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.listView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
@@ -133,12 +206,16 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.PWbox);
             this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Password Generator";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,9 +229,16 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn checkbox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AcctName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AccntPass;
     }
 }
 
--- PassGen/PassGen/Form1.cs
+++ PassGen/PassGen/Form1.cs
@@ -18,17 +18,32 @@ namespace PassGen {
             button1.Left = (this.ClientSize.Width - ((button1.Width + 30) + button1.Width)) / 2;
             button3.Left = (this.ClientSize.Width - (button3.Width - (button3.Width + 30))) / 2;
             button2.Left = (this.ClientSize.Width - button2.Width) / 2;
-            listView1.Left = (this.ClientSize.Width - listView1.Width) / 2;
+            dataGridView1.Left = (this.ClientSize.Width - dataGridView1.Width) / 2;
+            dataGridView1.AllowUserToOrderColumns = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            for(int i = 1; i < dataGridView1.ColumnCount; i++) {
+                dataGridView1.Columns[i].Width = (dataGridView1.Width - 27) / 2;
+            };
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e) {
             if(checkBox1.Checked) {
                 button1.Enabled = false;
                 PWbox.Enabled = true;
+                panel1.Enabled = false;
             } else {
                 button1.Enabled = true;
                 PWbox.Enabled = false;
+                panel1.Enabled = true;
             };
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            dataGridView1.Rows.Add(false, NameBox.Text, PWbox.Text);
         }
     }
 }
