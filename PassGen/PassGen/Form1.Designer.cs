ac352e8f4fc30e920b1a6da97fd071447a9a2958
William M-w <williammw.wmw@gmail.com>
2018-04-13 21:12:31 +0100

    Added UI

--- PassGen/PassGen/Form1.Designer.cs
+++ PassGen/PassGen/Form1.Designer.cs
@@ -23,13 +23,138 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.PWbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(218, 66);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(513, 20);
+            this.NameBox.TabIndex = 0;
+            // 
+            // PWbox
+            // 
+            this.PWbox.Enabled = false;
+            this.PWbox.Location = new System.Drawing.Point(218, 107);
+            this.PWbox.Name = "PWbox";
+            this.PWbox.Size = new System.Drawing.Size(513, 20);
+            this.PWbox.TabIndex = 0;
+            this.PWbox.Text = "PlaceHoldertext";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Account Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Account Name";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(700, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(88, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Manual Input";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Location = new System.Drawing.Point(366, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate Password";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Location = new System.Drawing.Point(30, 245);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(758, 164);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 28);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Delete selected.";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gainsboro;
+            this.button3.Location = new System.Drawing.Point(510, 168);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 42);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Save Login Details";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PWbox);
+            this.Controls.Add(this.NameBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox PWbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- PassGen/PassGen/Form1.cs
+++ PassGen/PassGen/Form1.cs
@@ -13,5 +13,22 @@ namespace PassGen {
         public Form1() {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            button1.Left = (this.ClientSize.Width - ((button1.Width + 30) + button1.Width)) / 2;
+            button3.Left = (this.ClientSize.Width - (button3.Width - (button3.Width + 30))) / 2;
+            button2.Left = (this.ClientSize.Width - button2.Width) / 2;
+            listView1.Left = (this.ClientSize.Width - listView1.Width) / 2;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            if(checkBox1.Checked) {
+                button1.Enabled = false;
+                PWbox.Enabled = true;
+            } else {
+                button1.Enabled = true;
+                PWbox.Enabled = false;
+            };
+        }
     }
 }
